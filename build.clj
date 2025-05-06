56e513ec0a1b2151adfd0cb3a378622a0ca51a49
fgasperino <franco.gasperino@gmail.com>
2025-05-06 00:27:58 -0700

    * Moved to deps.edn.

---
+++ build.clj
@@ -0,0 +1,15 @@
+(ns build
+  (:refer-clojure :exclude [compile])
+  (:require [clojure.tools.build.api :as b]))
+
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+
+(defn compile
+  [_]
+  (b/delete {:path "target"})
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src/main/clojure"]
+                  :class-dir class-dir
+                  :filter-nses '[byte-transforms]
+                  :ns-compile '[byte-transforms]}))
