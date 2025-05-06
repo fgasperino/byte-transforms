dc5679bb05ae64dd94198fc61b2232719cf917c3
fgasperino <franco.gasperino@gmail.com>
2025-05-06 00:06:38 -0700

    Bumped dependency versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clj-commons/byte-transforms (or (System/getenv "PROJECT_VERSION") "0.2.1")
+(defproject org.clj-commons/byte-transforms (or (System/getenv "PROJECT_VERSION") "0.3.0")
   :description "Methods for hashing, compressing, and encoding bytes."
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
@@ -6,12 +6,12 @@
                                     :username :env/clojars_username
                                     :password :env/clojars_password
                                     :sign-releases true}]]
-  :dependencies [[byte-streams "0.2.0"]
-                 [org.xerial.snappy/snappy-java "1.1.8.4"]
-                 [commons-codec/commons-codec "1.10"]
-                 [net.jpountz.lz4/lz4 "1.3"]
-                 [org.apache.commons/commons-compress "1.20"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clj-commons/byte-streams "0.3.4"]
+                 [org.xerial.snappy/snappy-java "1.1.10.7"]
+                 [commons-codec/commons-codec "1.18.0"]
+                 [org.lz4/lz4-java "1.8.0"]
+                 [org.apache.commons/commons-compress "1.27.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.12.0"]
                                   [criterium "0.4.6"]
                                   [org.clojure/test.check "1.1.0"]
                                   [codox-md "0.2.0" :exclusions [org.clojure/clojure]]]}
